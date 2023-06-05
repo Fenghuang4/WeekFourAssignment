ad32d72fd33dd08f54ba8632c57e2ff614eadf88
A <team@superhornets.org>
2023-06-05 16:30:01 -0500

    removed duplication

--- src/WeekFourAssignment.java
+++ src/WeekFourAssignment.java
@@ -6,18 +6,19 @@ public class WeekFourAssignment {
 
         String primaryPhone = "555-123-1245";
         String secondaryPhone = "555.123.1246";
-        
+
         // Print results.
-        System.out.print("Name: " + name(firstName,lastName)+"\nPrimary Phone: "
+        System.out.print("Name: " + getFullName(firstName,lastName)+"\nPrimary Phone: "
         + phone(primaryPhone)+"\nSecondary Phone: " + phone(secondaryPhone));
     }
 
     //method to sanitize names
-    public static String name(String firstNameInput, String lastNameInput) {
-        return firstNameInput.substring(0,1).toUpperCase() + firstNameInput.substring(1)
-                + " " + lastNameInput.substring(0,1).toUpperCase() + lastNameInput.substring(1);
+    public static String getFullName(String firstNameInput, String lastNameInput) {
+        return formatName(firstNameInput) + " " + formatName(lastNameInput);
     }
-
+    public static String formatName(String nameInput) {
+        return nameInput.substring(0, 1).toUpperCase() + nameInput.substring(1);
+    }
     //method to sanitize phone numbers
     public static String phone(String phoneInput) {
         return phoneInput.replace("-","").replace("(","").replace(
